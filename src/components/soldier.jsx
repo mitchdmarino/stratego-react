e36d05c01a0799eb11a3a0cecdf06f1eea4c011b
mitchdmarino <mitchdmarino@gmail.com>
2022-08-20 14:57:15 -0700

    soldier visual updates

--- src/components/soldier.jsx
+++ src/components/soldier.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Piece = styled.div`
+    height: 40px;
+    width: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    border-left: 8px solid black;
+    border-right: 8px solid black;
+`
 
 
-export default function Soldier ({piece}) {    
+
+export default function Soldier ({piece}) {   
+    let rank = null
+    let color = piece.color
+    let bgColor = '#DFDFDF' 
+    if (color === 'red') {
+        color = '#DB222A'
+        bgColor = '#F9B5AC'
+    } else {
+        color = '#0E34A0'
+        bgColor = '#BFDBF7'
+    }
+    if (piece.revealed) {
+        rank = piece.rank
+        if (piece.rank === 'b') {
+            rank = '💣'
+        }
+        if (piece.rank === 'f') {
+            rank = '🏁'
+        }
+    } else {
+        bgColor = '#DB222A'
+    }
+     
     return (
-        <div 
-            style={{backgroundColor: piece.color, height: 40, width: 40}} 
-            className={`${piece.color}-piece`}
+        <Piece 
+            style={{borderColor: color, backgroundColor: bgColor }} 
+            className={`${piece.color}-piece`
+            }
             >
-            
-            {piece.revealed ? piece.rank: ''}
-        
-        </div>
+            {rank}
+        </Piece>
     )
 }
